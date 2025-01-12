4588a9cd191f467b91f09df5759ad1439f1012ed
PaperMushrooms <dreems2reality@gmail.com>
2025-01-12 17:44:36 -0500

    Update

--- home/packages.nix
+++ home/packages.nix
@@ -4,25 +4,25 @@
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
-      kitty
-      dunst
-      discord
-      gimp
-      ungoogled-chromium
-      telegram-desktop
-      neofetch
-      htop
-      payload-dumper-go
-      starship
-      powerline
-      powerline-fonts
-      terminus_font  	 
-	    (wineWowPackages.full.override {
-     	  wineRelease = "staging";
-     	  mingwSupport = true;
-   	  })
-   	  winetricks
-      qt6.qttools
+      kitty                                            # Terminal preference
+      dunst                                            # Notification Manager
+      discord                                          # Messenger
+      gimp                                             # Photo Editor
+      ungoogled-chromium                               # Web Browser
+      telegram-desktop                                 # Private Messenger
+      neofetch                                         # System Specs Reader
+      btop                                             # System Monitor
+      payload-dumper-go                                # Payload.bin dumper
+      starship                                         # Terminal Customization
+      powerline                                        # Terminal Customization
+      powerline-fonts                                  # Terminal Customization
+      terminus_font  	                                 # Terminal Customization
+	    (wineWowPackages.full.override {                 # Wine config  
+     	  wineRelease = "staging";                       # for       
+     	  mingwSupport = true;                           # Windows compatibility.
+   	  })                                               #
+   	  winetricks                                       # extra Wine add-ons for Windows compatibility.
+      qt6.qttools                                      # Add-ons for GNOME
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
