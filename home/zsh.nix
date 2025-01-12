fc915917e2200c7a7525037a0ee7d78c1834cdf6
PaperMushrooms <dreems2reality@gmail.com>
2025-01-12 17:27:32 -0500

    Update

--- home/zsh.nix
+++ home/zsh.nix
@@ -8,6 +8,7 @@
     shellAliases = {
       update = "sudo nixos-rebuild switch";
       upgrade = "sudo nix flake update --flake /etc/nixos/ && sudo nixos-rebuild switch";
+      newdots = "cd /etc/nixos && git add . && git commit -m 'Update' && git push -u origin main && cd ";
     };
   };
 }
