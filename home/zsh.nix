caf7011d173daf2b4052d04994dbad46c2359a59
PaperMushrooms <dreems2reality@gmail.com>
2025-01-13 03:46:08 -0500

    Update

--- home/zsh.nix
+++ home/zsh.nix
@@ -26,23 +26,24 @@
   # Enable Font Configuration for NixOS
   fonts.fontconfig.enable = true;
 
-  # Install Terminal Add-on Packages
+  # Install Terminal Add-on Packages and Fontsv
   home.packages = with pkgs; [
     starship
     powerline
     powerline-fonts
+    jetbrains-mono
   ];
 
   # Enable and Configure Starship
   programs.starship = {
     enable = true;
     settings = {
-      add_newline = false;
+      add_newline = true;
       command_timeout = 1300;
       scan_timeout = 50;
       format = "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
       character = {
-        success_symbol = "[](bold green) ";
+        success_symbol = "[$](bold green) ";
         error_symbol = "[✗](bold red) ";
       };
     };
