6e9a02bffe99737939966d85abf235dc5ccf428c
PaperMushrooms <dreems2reality@gmail.com>
2025-01-13 02:45:56 -0500

    Update

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   programs.zsh = {
     enable = true;
@@ -10,5 +12,18 @@
       upgrade = "sudo nix flake update --flake /etc/nixos/ && sudo nixos-rebuild switch";
       newdots = "cd /etc/nixos && git add . && git commit -m 'Update' && git push -u origin main && cd ";
     };
+
+    plugins = [
+      {
+        # will source zsh-autosuggestions.plugin.zsh
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.4.0";
+          sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+        };
+      }
+    ];
   };
 }
