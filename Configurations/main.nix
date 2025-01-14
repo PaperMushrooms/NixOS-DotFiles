4447fa01e243d91c0e862e492dd1c753220763f8
PaperMushrooms <dreems2reality@gmail.com>
2025-01-14 06:20:32 -0500

    Update

--- Configurations/main.nix
+++ Configurations/main.nix
@@ -67,6 +67,9 @@
   # Install Android Tools.
   programs.adb.enable = true;
 
+  # Enable WayDroid Android Emulator
+  virtualisation.waydroid.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.  # programs.mtr.enable = true;
   # programs.gnupg.agent = {
