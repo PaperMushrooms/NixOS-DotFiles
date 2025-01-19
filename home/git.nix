a21a6a81617ef55b934c2749be3758ffae92af79
PaperMushrooms <dreems2reality@gmail.com>
2025-01-19 17:04:30 -0500

    Update

--- home/git.nix
+++ home/git.nix
@@ -8,16 +8,6 @@
     # Set Default Branch as main
     extraConfig = {
       init.defaultBranch = "main";
-      
-      Host github.com;
-        User git;
-        IdentityFile ~/.ssh/GitHub;
-        IdentitiesOnly yes;
-
-      Host gitlab.com;
-        User git;
-        IdentityFile ~/.ssh/GitHub;
-        IdentitiesOnly yes;
     };
   };
 }
