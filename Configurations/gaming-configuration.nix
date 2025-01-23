2e6871d507449e423543c85e673615c9cbd2095f
PaperMushrooms <dreems2reality@gmail.com>
2025-01-23 17:35:35 -0500

    Update

--- Configurations/gaming-configuration.nix
+++ Configurations/gaming-configuration.nix
@@ -11,12 +11,13 @@
 
   # Enable Gaming Related Packages
   environment.systemPackages = with pkgs; [
+  lutris             # Open-source game manager for Linux
+  bottles            # Tool for Windows software on Linux
   vulkan-tools       # Vulkan utilities like vulkaninfo
   vulkan-loader      # Vulkan loader
   mesa               # Open-source AMD drivers
-  minicom            # [To be listed...]
-  mangohud
-  lutris
+  minicom            # [ To be listed... ]
+  mangohud           # System performance heads-up display for OpenGL and Vulkan applications
   gamescope
   vkd3d
   dxvk
