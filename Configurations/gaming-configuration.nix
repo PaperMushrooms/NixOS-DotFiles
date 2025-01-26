1378d9e40915e927dfc1b2cc81153ec9ba3ea4cd
PaperMushrooms <dreems2reality@gmail.com>
2025-01-26 14:30:44 -0500

    Update

--- Configurations/gaming-configuration.nix
+++ Configurations/gaming-configuration.nix
@@ -13,7 +13,6 @@
   environment.systemPackages = with pkgs; [
   xivlauncher        # Custom launcher for Final Fantasy XIV
   lutris             # Open-source game manager for Linux
-  winePackages.ge    # 
   vulkan-tools       # Vulkan utilities like vulkaninfo
   vulkan-loader      # Vulkan loader
   mesa               # Open-source AMD drivers
