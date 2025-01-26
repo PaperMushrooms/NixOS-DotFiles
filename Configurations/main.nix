c347f69fbbf0085553e09bd5238481d1cf2fa88f
PaperMushrooms <dreems2reality@gmail.com>
2025-01-26 16:10:29 -0500

    Update

--- Configurations/main.nix
+++ Configurations/main.nix
@@ -94,6 +94,9 @@
   # Force Plasma SSh Keypass Instead of GNOME
   programs.ssh.askPassword = lib.mkForce "/nix/store/awb6dzl5kcwi2910frjcw0b96988fp2b-ksshaskpass-6.2.4/bin/ksshaskpass";
 
+  # Enable dconf
+  programs.dconf.enable = true;
+
   # GNOME Desktop Integration
   qt = {
     enable = true;
