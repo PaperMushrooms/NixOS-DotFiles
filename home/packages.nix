e2e65ddd170c5bb61b10fa08764764959bfe86c5
PaperMushrooms <dreems2reality@gmail.com>
2025-02-01 15:33:47 -0500

    Update

--- home/packages.nix
+++ home/packages.nix
@@ -7,7 +7,7 @@
       kitty                                            # Terminal preference
       dunst                                            # Notification Manager
       discord                                          # Messenger
-                                                 # Note Taking Software
+      notion-app-enhanced                              # Note Taking Software
       gimp                                             # Photo Editor
       ungoogled-chromium                               # Web Browser
       telegram-desktop                                 # Private Messenger
