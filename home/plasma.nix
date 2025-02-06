d57a4a9df04e5d73dc2a8fddfc7ad2e8d8908753
PaperMushrooms <dreems2reality@gmail.com>
2025-02-06 06:35:42 -0500

    Update

--- home/plasma.nix
+++ home/plasma.nix
@@ -18,5 +18,19 @@
         theme = "Breeze";
       };
     };
+    settings = {
+      panels = [
+        {
+          location = "bottom";  # Options: "top", "bottom", "left", "right"
+          alignment = "center"; # Options: "left", "center", "right"
+          widgets = [
+            "org.kde.plasma.kickoff"  # Application Launcher
+            "org.kde.plasma.taskmanager"  # Task Manager
+            "org.kde.plasma.systemtray"  # System Tray
+            "org.kde.plasma.digitalclock"  # Clock
+          ];
+        }
+      ];
+    };
   };
 }
