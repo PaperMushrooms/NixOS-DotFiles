1d40dc3c3da0129dade9dc10ab601f525197b453
PaperMushrooms <dreems2reality@gmail.com>
2025-02-07 07:15:09 -0500

    Update

--- system/bootloader-config.nix
+++ system/bootloader-config.nix
@@ -1,8 +1,17 @@
+#{
+#  # boot.loader.systemd-boot.enable = true; # Enable systemd-boot
+#    boot.loader.efi.canTouchEfiVariables = true;
+#    boot.loader.grub.enable = true; # Enable Grub
+#    boot.loader.grub.devices = [ "nodev" ];
+#    boot.loader.grub.efiSupport = true;
+#    boot.loader.grub.useOSProber = true;
+#}
+
 {
-  # boot.loader.systemd-boot.enable = true; # Enable systemd-boot
-    boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.grub.enable = true; # Enable Grub
-    boot.loader.grub.devices = [ "nodev" ];
-    boot.loader.grub.efiSupport = true;
-    boot.loader.grub.useOSProber = true;
+  boot.loader.grub = {
+      enable = true;
+      devices = [ "nodev" ];
+      efiSupport = true;
+      theme = pkgs.breeze;
+  };
 }
