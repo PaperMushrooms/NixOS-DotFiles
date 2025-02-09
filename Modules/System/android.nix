b05f7c9f6e1f5d9272f244b40f685cecd1a65204
PaperMushrooms <dreems2reality@gmail.com>
2025-02-09 09:03:38 -0500

    Update

--- Modules/System/android.nix
+++ Modules/System/android.nix
@@ -8,161 +8,161 @@
   services.usbmuxd.enable = true;
     
   environment.systemPackages = with pkgs; [
-    
-    pkgs.writeShellScriptBin "McLaren-crDroid7-A11" ''
+
+    (pkgs.writeShellScriptBin "McLaren-crDroid7-A11" ''
         cd ~/Android/McLaren/crDroid7-A11/
         bash install.sh
         cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "McLaren-crDroid8-A12" ''
+    (pkgs.writeShellScriptBin "McLaren-crDroid8-A12" ''
       cd ~/Android/McLaren/crDroid8-A12/
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "McLaren-crDroid9-A13" ''
+    (pkgs.writeShellScriptBin "McLaren-crDroid9-A13" ''
       cd ~/Android/McLaren/crDroid9-A13/
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "McLaren-crDroid10-A14" ''
+    (pkgs.writeShellScriptBin "McLaren-crDroid10-A14" ''
       cd ~/Android/McLaren/crDroid10-A14/
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "OnePlus6T-crDroid7-A11" ''
+    (pkgs.writeShellScriptBin "OnePlus6T-crDroid7-A11" ''
       cd ~/Android/OnePlus6T/crDroid7-A11
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "OnePlus6T-crDroid8-A12" ''
+    (pkgs.writeShellScriptBin "OnePlus6T-crDroid8-A12" ''
       cd ~/Android/OnePlus6T/crDroid7-A11
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "OnePlus6T-crDroid9-A13" ''
+    (pkgs.writeShellScriptBin "OnePlus6T-crDroid9-A13" ''
       cd ~/Android/OnePlus6T/crDroid9-A13
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "OnePlus6T-crDroid10-A14" ''
+    (pkgs.writeShellScriptBin "OnePlus6T-crDroid10-A14" ''
         cd ~/Android/OnePlus6T/crDroid10-A14
         bash install.sh
         cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "OnePlus9-crDroid9-A13" ''
+    (pkgs.writeShellScriptBin "OnePlus9-crDroid9-A13" ''
       cd ~/Android/OnePlus9/crDroid9-A13
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "OnePlus9-crDroid10-A14" ''
+    (pkgs.writeShellScriptBin "OnePlus9-crDroid10-A14" ''
       cd ~/Android/OnePlus9/crDroid10-A14
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel5-crDroid9-A13" ''
+    (pkgs.writeShellScriptBin "Pixel5-crDroid9-A13" ''
       cd ~/Android/Pixel5/crDroid9-A13
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel5-crDroid10-A14" ''
+    (pkgs.writeShellScriptBin "Pixel5-crDroid10-A14" ''
       cd ~/Android/Pixel5/crDroid10-A14
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel5-GrapheneOS-A14" ''
+    (pkgs.writeShellScriptBin "Pixel5-GrapheneOS-A14" ''
       cd ~/Android/Pixel5/GrapheneOS-A14
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel5-OEM-A11" ''
+    (pkgs.writeShellScriptBin "Pixel5-OEM-A11" ''
       cd ~/Android/Pixel5/OEM-A11-Latest
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel5-OEM-A12" ''
+    (pkgs.writeShellScriptBin "Pixel5-OEM-A12" ''
       cd ~/Android/Pixel5/OEM-A12-Latest
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel5-OEM-A13" ''
+    (pkgs.writeShellScriptBin "Pixel5-OEM-A13" ''
       cd ~/Android/Pixel5/OEM-A13-Latest
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel5-OEM-A14" ''
+    (pkgs.writeShellScriptBin "Pixel5-OEM-A14" ''
       cd ~/Android/Pixel5/OEM-A14-Latest
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel6-crDroid10-A14" ''
+    (pkgs.writeShellScriptBin "Pixel6-crDroid10-A14" ''
       cd ~/Android/Pixel6/crDroid10-A14
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel6-OEM-A13" ''
+    (pkgs.writeShellScriptBin "Pixel6-OEM-A13" ''
       cd ~/Android/Pixel6/OEM-A13-Magisk
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel6a-DerpFest-A14" ''
+    (pkgs.writeShellScriptBin "Pixel6a-DerpFest-A14" ''
       cd ~/Android/Pixel6a/DerpFest-A14
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel6a-OEM-A13" ''
+    (pkgs.writeShellScriptBin "Pixel6a-OEM-A13" ''
       cd ~/Android/Pixel6a/OEM-A13-Latest
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel7a-crDroid9-A13" ''
+    (pkgs.writeShellScriptBin "Pixel7a-crDroid9-A13" ''
       cd ~/Android/Pixel7a/crDroid9-A13
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel7a-crDroid10-A14" ''
+    (pkgs.writeShellScriptBin "Pixel7a-crDroid10-A14" ''
       cd ~/Android/Pixel7a/crDroid10-A14
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel7a-OEM-A13" ''
+    (pkgs.writeShellScriptBin "Pixel7a-OEM-A13" ''
       cd ~/Android/Pixel7a/OEM-A13-Latest
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "Pixel7a-OEM-A14" ''
+    (pkgs.writeShellScriptBin "Pixel7a-OEM-A14" ''
       cd ~/Android/Pixel7a/OEM-A14-Latest
       bash install.sh
       cd 
-    ''
+    '')
 
-    pkgs.writeShellScriptBin "PokeApps" ''
+    (pkgs.writeShellScriptBin "PokeApps" ''
       cd ~/Android/Apps
       bash install.sh
       cd 
-    ''  
+    '')  
   ];
 }
