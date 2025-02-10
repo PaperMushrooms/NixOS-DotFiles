def98f42466865a8e7d7021d054b8a96b0d2c0ff
PaperMushrooms <dreems2reality@gmail.com>
2025-02-09 23:10:59 -0500

    Update

--- recovery/configuration.nix
+++ recovery/configuration.nix
@@ -1,9 +1,19 @@
 { pkgs, modulesPath, ... }: {
 
-{
   imports = [
-    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
+
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+    networkmanager
+    wpa_supplicant
+  ];
+  
+  systemd.services.NetworkManager.enable = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
