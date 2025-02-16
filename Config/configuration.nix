88c396b99502d377e372f677980c1cfb8feccef0
PaperMushrooms <dreems2reality@gmail.com>
2025-02-16 08:49:14 -0500

    Update

--- Config/configuration.nix
+++ Config/configuration.nix
@@ -51,7 +51,9 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager =
+    enable = true;
+  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
