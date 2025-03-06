413b04e344243176a364ec086ab3d101f6cb21e8
PaperMushrooms <dreems2reality@gmail.com>
2025-03-06 05:08:48 -0500

    Update

--- Modules/System/virtualisation.nix
+++ Modules/System/virtualisation.nix
@@ -9,7 +9,7 @@ in {
     virtualisation = mkEnableOption "Enable Virtualisation module for virtual machines";
   };
 
-  config = mkIf cfg {
+  config = mkIf cfg.enable {
     programs.virt-manager.enable = true; 
     users.groups.libvirtd.members = ["nixon"];
     virtualisation.libvirtd.enable = true;
