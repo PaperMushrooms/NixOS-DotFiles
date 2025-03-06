643d8f910fa8d2e22670f69e7087f68b1ba615d4
PaperMushrooms <dreems2reality@gmail.com>
2025-03-06 06:04:14 -0500

    Update

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
           ./Config/configuration.nix
           ./Modules/System/default.nix
 	  {
-	    System.ssh = true;
+	    #System.ssh = true;
 	    System.android = true;
 	    System.virtualisation = true;
 	    System.gaming = true;
