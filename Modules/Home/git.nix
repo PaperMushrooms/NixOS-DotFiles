3fbb6219556737d4d3a86fb696fc27a72119d389
PaperMushrooms <dreems2reality@gmail.com>
2025-03-06 10:10:20 -0500

    Update

--- Modules/Home/git.nix
+++ Modules/Home/git.nix
@@ -1,13 +1,22 @@
-{
-  # Enable and Configure Git
-  programs.git = {
-    enable = true;
-    userName = "PaperMushrooms";
-    userEmail = "dreems2reality@gmail.com";
+{ config, lib, pkgs, ... }: with lib; {
 
-    # Set Default Branch as main
-    extraConfig = {
-      init.defaultBranch = "main";
+  options = {
+    git.enable = 
+      mkEnableOption "Enables Git"
+  };
+
+  config = mkIf config.git.enable {
+
+    # Enable and Configure Git
+    programs.git = {
+      enable = true;
+      userName = "PaperMushrooms";
+      userEmail = "dreems2reality@gmail.com";
+
+      # Set Default Branch as main
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
     };
   };
 }
