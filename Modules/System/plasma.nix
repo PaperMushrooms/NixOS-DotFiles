d784cfa78e2031e414c89330ca9ebae7f52457b1
PaperMushrooms <dreems2reality@gmail.com>
2025-03-09 12:48:27 -0400

    Update

--- Modules/System/plasma.nix
+++ Modules/System/plasma.nix
@@ -4,6 +4,8 @@
   # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
 
+  services.displayManager.sddm.enable = true;
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 }
