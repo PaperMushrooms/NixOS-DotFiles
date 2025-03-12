37d2ad7f7c4d0816153f93165d6987b7e7887bfc
PaperMushrooms <dreems2reality@gmail.com>
2025-03-12 15:08:01 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
 	    virtualisationconf.enable = true;
 	    gaming.enable = true;
 	    gnomeconf.enable = true;
-	    # libreoffice.enable = true;
+	    libreoffice.enable = true;
 	    hyprconf.enable = true;
 	  }
 	  home-manager.nixosModules.home-manager # Home-Manager Module
