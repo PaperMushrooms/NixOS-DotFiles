18bd76b572bdcabfa6850c0bb19ffe6c1de5c934
PaperMushrooms <dreems2reality@gmail.com>
2025-03-16 21:50:00 -0400

    Update

--- Modules/System/plasma.nix
+++ Modules/System/plasma.nix
@@ -3,6 +3,7 @@
   options = {
     plasmahome.enable =
       mkEnableOption "Enable and Configure Plasma6";
+ };
 
   # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
