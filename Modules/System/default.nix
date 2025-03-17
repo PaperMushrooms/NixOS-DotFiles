b382a43728157f58cabe075388415d55609a62e2
PaperMushrooms <dreems2reality@gmail.com>
2025-03-16 22:01:23 -0400

    Update

--- Modules/System/default.nix
+++ Modules/System/default.nix
@@ -2,7 +2,7 @@
   imports = [
     ./gaming.nix
     ./bootloader.nix
-    ../../modules/plasma.nix
+    ./plasma.nix
     ./ssh.nix
     ./android.nix
     ./virtualisation.nix
--- Modules/System/plasma.nix
+++ Modules/System/plasma.nix
@@ -1,11 +1,34 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }: with lib; {
 
-with lib; {
   options = {
-    plasmahome.enable = mkEnableOption "Enable and Configure Plasma6";
+    plasmahome.enable =
+      mkEnableOption "Enable and Configure Plasma6";
   };
 
-  config = mkIf config.plasmahome.enable {
+  config = mkIf config.plasmahome.enable { 
+    # Configure Plasma Theme
+    programs.plasma = {
+      enable = true;
+      workspace = {
+        colorScheme = "BreezeDark"; # Enables dark mode
+        
+        windowDecorations = {
+          library = "org.kde.breeze";
+          theme = "Breeze";
+        };
+  
+        splashScreen = {
+          theme = "None";
+        };
+  
+        theme = "BreezeDark";
+  
+        cursor= {
+          theme = "Breeze";
+        };
+      };
+    };
+
     services.desktopManager.plasma6.enable = true;
     services.displayManager.sddm.enable = true;
     services.xserver.enable = true;
