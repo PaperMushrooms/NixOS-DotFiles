dbab4e2a9f2e62afa58a3a971846237cc3db59be
PaperMushrooms <dreems2reality@gmail.com>
2025-03-16 22:58:16 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     nixpkgs.url = {
-      "github:nixos/nixpkgs?ref=nixos-unstable";
-      "github:NixOS/nixpkgs/21808d22b1cda1898b71cf1a1beb524a97add2c4";
+      github:nixos/nixpkgs?ref=nixos-unstable;
+      github:NixOS/nixpkgs/21808d22b1cda1898b71cf1a1beb524a97add2c4;
     };
 
     home-manager = {
