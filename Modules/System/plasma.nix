67da49757492f978ddba553876e009183718838f
PaperMushrooms <dreems2reality@gmail.com>
2025-03-16 21:56:58 -0400

    Update

--- Modules/System/plasma.nix
+++ Modules/System/plasma.nix
@@ -1,12 +1,11 @@
-{ config, lib, pkgs, ... }: with lib; {
+{ config, lib, pkgs, ... }:
 
+with lib; {
   options = {
-    plasmahome.enable =
-      mkEnableOption "Enable and Configure Plasma6";
- };
+    plasmahome.enable = mkEnableOption "Enable and Configure Plasma6";
+  };
 
-  config = mkIf config.plasmahome.enable { 
-    # Configure Plasma Theme
+  config = mkIf config.plasmahome.enable {
     programs.plasma = {
       enable = true;
       workspace = {
@@ -23,7 +22,7 @@
   
         theme = "BreezeDark";
   
-        cursor= {
+        cursor = {
           theme = "Breeze";
         };
       };
@@ -32,6 +31,6 @@
     services.desktopManager.plasma6.enable = true;
     services.displayManager.sddm.enable = true;
     services.xserver.enable = true;
-
   };
 }
+
