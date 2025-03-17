296f4876efdecbc758fc71607a09de1381d3d5e5
PaperMushrooms <dreems2reality@gmail.com>
2025-03-16 22:16:40 -0400

    Update

--- Modules/System/plasma.nix
+++ Modules/System/plasma.nix
@@ -5,33 +5,37 @@
       mkEnableOption "Enable and Configure Plasma6";
   };
 
-  config = mkIf config.plasmahome.enable { 
-    # Configure Plasma Theme
-    programs.plasma = {
-      enable = true;
-      workspace = {
-        colorScheme = "BreezeDark"; # Enables dark mode
-        
-        windowDecorations = {
-          library = "org.kde.breeze";
-          theme = "Breeze";
-        };
-  
-        splashScreen = {
-          theme = "None";
-        };
-  
-        theme = "BreezeDark";
-  
-        cursor= {
-          theme = "Breeze";
-        };
-      };
-    };
-
+  config = mkIf config.plasmahome.enable {
+    # System-level configuration
     services.desktopManager.plasma6.enable = true;
     services.displayManager.sddm.enable = true;
     services.xserver.enable = true;
+
+    # Home-Manager configuration
+    home-manager.users.${config.home.username} = mkIf (config ? home-manager) {
+      # Configure Plasma Theme
+      programs.plasma = {
+        enable = true;
+        workspace = {
+          colorScheme = "BreezeDark"; # Enables dark mode
+          
+          windowDecorations = {
+            library = "org.kde.breeze";
+            theme = "Breeze";
+          };
+  
+          splashScreen = {
+            theme = "None";
+          };
+  
+          theme = "BreezeDark";
+  
+          cursor= {
+            theme = "Breeze";
+          };
+        };
+      };
+    };
   };
 }
 
