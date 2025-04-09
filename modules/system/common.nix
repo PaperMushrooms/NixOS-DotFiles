4daeaa16d004949d42c6a401e58e71c1c31e7a50
PaperMushrooms <dreems2reality@gmail.com>
2025-04-09 10:19:05 -0400

    Update

---
+++ modules/system/common.nix
@@ -0,0 +1,29 @@
+{
+    # Enable Grub Bootloader Configuration
+    grubconf.enable = true;
+
+    # Enable Audio
+    audio.enable = true;
+    
+    # Enable SSH Configuration
+    ssh.enable = true;
+
+    # Enable Virtualisation Compatibility
+    virtualisationconf.enable = true;
+
+    # Enable Gaming Configuration and Tools
+    gaming.enable = true;
+
+    # Enable Gnome System-Level Configuration
+    gnomesys.enable = true;
+
+    # Enable LibreOffice Toolbox
+    libreoffice.enable = true;
+
+    # Enable Hyprland Configuration
+    hyprconf.enable = true;
+
+    # Enable Plasma6 System-Level Configuration
+    plasmasys.enable = true;
+}    
+
