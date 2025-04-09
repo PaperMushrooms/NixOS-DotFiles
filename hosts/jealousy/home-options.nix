01c6fd19d045e89813da360bb7afc3d463ff177c
PaperMushrooms <dreems2reality@gmail.com>
2025-04-09 10:46:12 -0400

    Update

---
+++ hosts/jealousy/home-options.nix
@@ -0,0 +1,9 @@
+}
+  vscodium.enable = true;
+  gnomehome.enable = true;
+  hyprhome.enable = true;
+  git.enable = true;
+  firefox.enable = true;
+  myshell.enable = true;
+  plasmahome.enable = true;
+}
