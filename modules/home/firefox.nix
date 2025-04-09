788845f7a3028d1bfd5567ce28ecbcdfcab7e7d5
PaperMushrooms <dreems2reality@gmail.com>
2025-04-09 08:29:14 -0400

    Update

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -21,46 +21,9 @@
             "ui.systemUsesDarkTheme" = 1;
           };
 
-          bookmarks = [
-            {
-              name = "Gmail";
-              tags = [ "Email" ];
-              keyword = "gmail";
-              url = "https://mail.google.com/";
-            }
-            {
-              name = "Google Drive";
-              tags = [ "Cloud" ];
-              keyword = "drive";
-              url = "https://drive.google.com/";
-            }
-            {
-              name = "Ebay";
-              tags = [ "Shopping" ];
-              keyword = "drive";
-              url = "https://www.ebay.com/";
-            }
-            {
-              name = "ChatGPT";
-              tags = [ "Artificial Intelligence" ];
-              keyword = "GPT";
-              url = "https://chatgpt.com/";
-            }
-            {
-              name = "GitHub";
-              tags = [ "Development" ];
-              keyword = "Git";
-              url = "https://github.com/";
-            }
-            {
-              name = "NixOS & Flakes Book";
-              tags = [ "Education" ];
-              keyword = "NixManual";
-              url = "https://nixos-and-flakes.thiscute.world/";
-            }
-            {
-            toolbar = true;
-            bookmarks = [
+          bookmarks = {
+	    force = true;
+	    settings = [
               {
                 name = "Gmail";
                 tags = [ "Email" ];
@@ -97,7 +60,47 @@
                 keyword = "NixManual";
                 url = "https://nixos-and-flakes.thiscute.world/";
               }
-            ];
+              {
+              toolbar = true;
+              bookmarks = [
+                {
+                  name = "Gmail";
+                  tags = [ "Email" ];
+                  keyword = "gmail";
+                  url = "https://mail.google.com/";
+                }
+                {
+                  name = "Google Drive";
+                  tags = [ "Cloud" ];
+                  keyword = "drive";
+                  url = "https://drive.google.com/";
+                }
+                {
+                  name = "Ebay";
+                  tags = [ "Shopping" ];
+                  keyword = "drive";
+                  url = "https://www.ebay.com/";
+                }
+                {
+                  name = "ChatGPT";
+                  tags = [ "Artificial Intelligence" ];
+                  keyword = "GPT";
+                  url = "https://chatgpt.com/";
+                }
+                {
+                  name = "GitHub";
+                  tags = [ "Development" ];
+                  keyword = "Git";
+                  url = "https://github.com/";
+                }
+                {
+                  name = "NixOS & Flakes Book";
+                  tags = [ "Education" ];
+                  keyword = "NixManual";
+                  url = "https://nixos-and-flakes.thiscute.world/";
+                }
+              ];
+	     };
            }
           ];
           extensions.packages = with inputs.firefox-addons.packages."x86_64-linux"; [
