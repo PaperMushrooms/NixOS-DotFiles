d09b2a44a3e33b3fcfda81e7cd375f14427f7169
PaperMushrooms <dreems2reality@gmail.com>
2025-05-30 17:26:28 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -12,86 +12,87 @@
     nix-on-droid = {
       url = "github:nix-community/nix-on-droid/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-      plasma-manager = {
-        url = "github:nix-community/plasma-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-        inputs.home-manager.follows = "home-manager";
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nix-on-droid, home-manager, plasma-manager, hyprland, ... }@inputs: {
+
+    nixosConfigurations = {
+      jealousy = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        specialArgs = { inherit inputs; };
+
+        modules = [
+          ./hosts/jealousy/system
+          ./hosts/jealousy/system/options.nix
+          ./modules/system
+
+          home-manager.nixosModules.home-manager # Home-Manager Module
+
+          {
+            home-manager = {
+              useUserPackages = true;
+              sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+              extraSpecialArgs = { inherit inputs; };
+              users = {
+                jealousy = import ./hosts/jealousy/home/home.nix;
+              };
+            };
+          }
+        ];
       };
 
-      hyprland.url = "github:hyprwm/Hyprland";
+      tuffy = nixpkgs.lib.nixosSystem {
+        system = "x86-linux";
 
-      firefox-addons = {
-        url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-        inputs.nixpkgs.follows = "nixpkgs";
+        specialArgs = { inherit inputs; };
+
+        modules = [
+          ./hosts/tuffy/system
+          ./hosts/tuffy/system/options.nix
+          ./modules/system
+
+          home-manager.nixosModules.home-manager # Home-Manager Module
+
+          {
+            home-manager = {
+              useUserPackages = true;
+              sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+              extraSpecialArgs = { inherit inputs; };
+              users = {
+                tuffy = import ./hosts/tuffy/home/home.nix;
+              };
+            };
+          }
+        ];
+      };
+
+      recovery = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./recovery/configuration.nix
+        ];
       };
     };
 
-    outputs = { self, nixpkgs, nix-on-droid, home-manager, plasma-manager, hyprland, ... }@inputs: {
-
-      nixosConfigurations = {
-        jealousy = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-
-          specialArgs = { inherit inputs; };
-
-          modules = [
-            ./hosts/jealousy/system
-            ./hosts/jealousy/system/options.nix
-            ./modules/system
-
-            home-manager.nixosModules.home-manager # Home-Manager Module
-
-            {
-              home-manager = {
-                useUserPackages = true;
-                sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
-                extraSpecialArgs = { inherit inputs; };
-                users = {
-                  jealousy = import ./hosts/jealousy/home/home.nix;
-                };
-              };
-            }
-          ];
-        };
-
-        tuffy = nixpkgs.lib.nixosSystem {
-          system = "x86-linux";
-
-          specialArgs = { inherit inputs; };
-
-          modules = [
-            ./hosts/tuffy/system
-            ./hosts/tuffy/system/options.nix
-            ./modules/system
-
-            home-manager.nixosModules.home-manager # Home-Manager Module
-
-            {
-              home-manager = {
-                useUserPackages = true;
-                sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
-                extraSpecialArgs = { inherit inputs; };
-                users = {
-                  tuffy = import ./hosts/tuffy/home/home.nix;
-                };
-              };
-            }
-          ];
-        };
-
-        recovery = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./recovery/configuration.nix
-          ];
-        };
-      };
-
-      nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
-        pkgs = import nixpkgs { system = "aarch64-linux"; };
-        modules = [ ./nix-on-droid.nix ];
-      };
+    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+      pkgs = import nixpkgs { system = "aarch64-linux"; };
+      modules = [ ./nix-on-droid.nix ];
     };
-  }
+  };
+}
 
