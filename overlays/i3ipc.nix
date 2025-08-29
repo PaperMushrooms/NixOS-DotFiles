eaf72685e5cbb89ddb22ea12defcccb28b4067b5
PaperMushrooms <dreems2reality@gmail.com>
2025-08-28 23:08:32 -0400

    Update

--- overlays/i3ipc.nix
+++ overlays/i3ipc.nix
@@ -1,10 +1,14 @@
-[
-  (final: prev: {
-    python3Packages = prev.python3Packages // {
-      i3ipc = prev.python3Packages.i3ipc.overridePythonAttrs (old: {
-        doCheck = false;
-      });
-    };
-  })
-]
+self: super: {
+  i3ipc = super.python3Packages.buildPythonPackage rec {
+    pname = "i3ipc";
+    version = "2.2.1";
 
+    src = super.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+
+    propagatedBuildInputs = with super.python3Packages; [ ];
+    doCheck = false;
+  };
+}
