13bf50c909fffccff8a58f8f78f08c25eca81542
PaperMushrooms <dreems2reality@gmail.com>
2025-09-04 18:50:54 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My home system flake c: ";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/pull/438729/head";
 
     home-manager = {
       url = "github:nix-community/home-manager";
