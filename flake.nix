615542ac9fbe1d3a7760c81f8c6a47c3dfe6e675
PaperMushrooms <dreems2reality@gmail.com>
2025-09-08 07:26:04 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
           ./hosts/jealousy/system/options.nix
           ./modules/system
 
-          nvf.nixosModules.default
+          nvf.homeModules.default
 
           home-manager.nixosModules.home-manager # Home-Manager Module
 
