9465d78be301de3bd950f1b36a497bbe424b10ba
PaperMushrooms <dreems2reality@gmail.com>
2025-09-10 05:01:29 -0400

    Update

--- modules/home/desktop/hyprland/config.nix
+++ modules/home/desktop/hyprland/config.nix
@@ -20,11 +20,6 @@
       enable = true;
 
       settings = {
-
-        monitor = [
-          "eDP-1, 1920x1200@60, 0x0, 1"
-        ];
-
         exec-once = [ "bash /etc/nixos/modules/home/desktop/hyprland/start.sh" ];
       };
     };
