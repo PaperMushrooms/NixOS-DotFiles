8c4a2a6671bc56ad604c2cf9df4dc1c7fad99745
PaperMushrooms <dreems2reality@gmail.com>
2025-09-10 04:52:26 -0400

    Update

---
+++ modules/home/desktop/hyprland/monitors.nix
@@ -0,0 +1,13 @@
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+    settings = {
+
+      monitor = [
+        "eDP-1, 1920x1200@60, 0x0, 1"
+        "HDMI-A-1, auto, 0x0, 1"
+      ];
+
+    };
+  }
