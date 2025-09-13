748e0f52a264970a5a612677e0aa3b09017c0c29
PaperMushrooms <dreems2reality@gmail.com>
2025-09-13 06:01:54 -0400

    Update

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -5,6 +5,10 @@
 
     enable = true;
 
+    targets = {
+      kde.enable = true;
+    };
+
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
     image = ../../wallpapers/catppuccin-mocha/purple-horizon.jpg;
