46fa6b139c61a5d25aeb368ea9a7d8484d8fe8b9
PaperMushrooms <dreems2reality@gmail.com>
2025-09-13 06:59:10 -0400

    Update

--- modules/home/desktop/hyprland/waybar.nix
+++ modules/home/desktop/hyprland/waybar.nix
@@ -1,43 +1,41 @@
 { config, pkgs, ... }:
 
 {
-  programs.waybar =
-    {
-      enable = true;
+  programs.waybar = {
+    enable = true;
 
-      settings = {
-        mainBar = {
-          layer = "top";
-          position = "top";
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
 
-          battery = {
-            format = "{capacity}% {icon}";
-            format-icons = [ "" "" "" "" "" ];
-          };
+        battery = {
+          format = "{capacity}% {icon}";
+          format-icons = [ "" "" "" "" "" ];
+        };
 
-          modules-left = [ "hyprland/workspaces" ];
+        modules-left = [ "hyprland/workspaces" ];
 
-          modules-center = [ "clock" ];
+        modules-center = [ "clock" ];
 
-          modules-right = [
-            "tray"
-            "pulseaudio"
-            "battery"
-          ];
+        modules-right = [
+          "tray"
+          "pulseaudio"
+          "battery"
+        ];
 
-          clock = {
-            format = "{:%I:%M %p}";
-          };
+        clock = {
+          format = "{:%I:%M %p}";
         };
       };
-    }
+    };
+  };
 
-      fonts.packages = with pkgs;
-  [
-  font-awesome
+  fonts.packages = with pkgs; [
+    font-awesome
     font-awesome_4
     font-awesome_5
     font-awesome_6
     font-awesome_7
-    ];
-    }
+  ];
+}
