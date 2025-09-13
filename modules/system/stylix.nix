03f299473c1add7d06452b02cb27eec4d2f980b0
PaperMushrooms <dreems2reality@gmail.com>
2025-09-13 08:54:48 -0400

    Update

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -7,6 +7,11 @@
 
     autoEnable = true;
 
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+    };
+
     targets = { };
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
