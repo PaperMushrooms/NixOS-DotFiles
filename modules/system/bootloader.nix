89f90278ff402cf2ae4160b7b01aa9994ac0626b
PaperMushrooms <dreems2reality@gmail.com>
2025-09-30 14:52:36 -0400

    Update

--- modules/system/bootloader.nix
+++ modules/system/bootloader.nix
@@ -11,21 +11,20 @@
       pname = "hyperfluent-grub-theme";
       version = "1.0";
 
-      # Fetch a specific commit from HyperFluent repo
       src = builtins.fetchGit {
         url = "https://github.com/Coopydood/HyperFluent-GRUB-Theme.git";
-        rev = "0xlk2fsb6fav08lyv1xzvhzid2nmbs6g5sdq57jmyfx1m26agyg3;" # pinned commit
-          };
-
-        installPhase = ''
-          mkdir -p $out
-          cp -r HyperFluent $out/
-        '';
+        rev = "0xlk2fsb6fav08lyv1xzvhzid2nmbs6g5sdq57jmyfx1m26agyg3;";
       };
-    };
 
-    boot.loader.efi = {
-      canTouchEfiVariables = true; # write boot entry into firmware NVRAM
-      efiSysMountPoint = "/boot"; # → make sure your ESP is mounted here
+      installPhase = ''
+        mkdir -p $out
+        cp -r HyperFluent $out/
+      '';
     };
-  }
+  };
+
+  boot.loader.efi = {
+    canTouchEfiVariables = true;
+    efiSysMountPoint = "/boot";
+  };
+}
