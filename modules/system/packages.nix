a06b9ae4cd0029fb4bdaead1282497fcf529fea6
PaperMushrooms <dreems2reality@gmail.com>
2025-09-30 13:46:47 -0400

    Update

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -33,4 +33,14 @@
     winePackages.full
     winetricks
   ];
+
+  # Inside configuration.nix or your flake's module
+  services.udev.extraRules = ''
+    # Flipper Zero DFU Mode
+    SUBSYSTEM=="usb", ATTR{idVendor}=="0483", ATTR{idProduct}=="df11", MODE="0666", GROUP="dialout"
+
+    # Flipper Zero Serial Mode
+    SUBSYSTEM=="usb", ATTR{idVendor}=="0483", ATTR{idProduct}=="5740", MODE="0666", GROUP="dialout"
+  '';
+
 }
