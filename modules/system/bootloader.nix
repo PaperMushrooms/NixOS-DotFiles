d2571a1dca30e84e91c31feb0da1ae149541ab5f
PaperMushrooms <dreems2reality@gmail.com>
2025-09-30 14:41:21 -0400

    Update

--- modules/system/bootloader.nix
+++ modules/system/bootloader.nix
@@ -6,7 +6,17 @@
     devices = [ "nodev" ];
     efiSupport = true;
     useOSProber = true;
-    #   theme = "${hyperfluent-theme}/HyperFluent";
+    theme = pkgs.stdenv.mkDerivation {
+      pname = "distro-grub-themes";
+      version = "3.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "AdisonCavani";
+        repo = "distro-grub-themes";
+        rev = "v3.1";
+        hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+      };
+      installPhase = "cp -r customize/nixos $out";
+    };
   };
 
   boot.loader.efi = {
