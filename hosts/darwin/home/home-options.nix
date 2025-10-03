d9ba733f7d0ebd2cf11a68ef2ab767f6f8f5e9c1
Darwin <matthew@MacBookAir.lan>
2025-10-03 15:34:17 -0400

    Update

--- hosts/darwin/home/home-options.nix
+++
@@ -1,9 +0,0 @@
-{
-  # vscodium.enable = true;
-  # gnomehome.enable = true;
-  # hyprhome.enable = true;
-   git.enable = true;
-  # firefox.enable = true;
-   myshell.enable = true;
-  # plasmahome.enable = true;
-}
