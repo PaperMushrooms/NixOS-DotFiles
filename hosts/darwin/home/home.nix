538e3a55ab04f55279ec22d6c239cadbfa3f5f41
Darwin <matthew@MacBookAir.lan>
2025-10-04 11:03:50 -0400

    Update

--- hosts/darwin/home/home.nix
+++ hosts/darwin/home/home.nix
@@ -25,6 +25,14 @@
     };
   };
 
+  home.packages = with pkgs; [ 
+    discord
+  ];
+
+  nixpkgs.config = { 
+      allowUnfree = true;
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
