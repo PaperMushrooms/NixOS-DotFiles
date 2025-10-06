efb9542f52633b295a87803936a6cb6f24fdd9e3
Darwin <matthew@MacBookAir.lan>
2025-10-06 14:17:42 -0400

    Update

--- hosts/darwin/system/default.nix
+++ hosts/darwin/system/default.nix
@@ -14,10 +14,22 @@
     allowUnsupportedSystem = false;
   };
 
+  security.pam.services.sudo_local.touchIdAuth = true;
+
   nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  homebrew = {
+    enable = true;
+    casks = [
+      "rustdesk"
+      "qflipper"
+      "bambu-studio"
+    ];
+  };
 
   # Set Git commit hash for darwin-version.
   system = {
+    primaryUser = "matthew";
     configurationRevision = null;
     stateVersion = 6;
   };
