5f8e4772c2f73415258d15553856c7c4fc207dbf
Darwin <matthew@MacBookAir.lan>
2025-10-11 23:34:32 -0400

    Update

--- shells/PoGo-Root/scripts.nix
+++ shells/PoGo-Root/scripts.nix
@@ -1,6 +1,6 @@
 { pkgs }: [
   (pkgs.writeShellScriptBin "McLaren-crDroid7-A11" ''
-    cd ~/Android/McLaren/crDroid7-A11/
+    cd /home/{$USER}/Android/McLaren/crDroid7-A11/
     bash install.sh
     cd
   '')
