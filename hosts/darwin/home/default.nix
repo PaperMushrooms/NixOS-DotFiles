17a3f5ad33ae525f8ac4572b3f4ecc272e4be02c
Darwin <matthew@MacBookAir.lan>
2025-10-20 19:57:58 -0400

    Update

--- hosts/darwin/home/default.nix
+++ hosts/darwin/home/default.nix
@@ -20,8 +20,8 @@
     enable = true;
     shellAliases = {
       rebuild = "sudo darwin-rebuild switch --flake /etc/nix-darwin#darwin";
-      hellnix-update = "cd /etc/nix-darwin && sudo nix flake update && sudo darwin-rebuild switch --flake .#darwin && ssh -t satanix 'cd /etc/nixos/ && git pull && sudo nixos-rebuild switch --flake .#satanix' && ssh -t nixspawn 'cd /etc/nixos && git pull && sudo nixos-rebuild switch --flake .#nixspawn'";
-    };
+      hellnix-update = "sudo nix flake update /etc/nix-darwin && upload && sudo darwin-rebuild switch --flake /etc/nix-darwin#darwin && ssh -t satanix 'cd /etc/nixos/ && git pull && sudo nixos-rebuild switch --flake .#satanix' && ssh -t nixspawn 'cd /etc/nixos && git pull && sudo nixos-rebuild switch --flake .#nixspawn'";
+hellnix-sync = "cd /etc/nix-darwin git pull && sudo darwin-rebuild switch --flake /etc/nix-darwin#darwin && ssh -t satanix 'cd /etc/nixos/ && git pull && sudo nixos-rebuild switch --flake .#satanix' && ssh -t nixspawn 'cd /etc/nixos && git pull && sudo nixos-rebuild switch --flake .#nixspawn'";
   };
 
   home.packages = with pkgs; [
