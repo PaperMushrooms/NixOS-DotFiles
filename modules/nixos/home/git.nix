7872107e61573f86e026d02ef7b9cfa56c1c0f8c
PaperMushrooms <dreems2reality@gmail.com>
2025-10-20 07:47:37 -0400

    Update

--- modules/nixos/home/git.nix
+++ modules/nixos/home/git.nix
@@ -10,7 +10,7 @@ with lib; {
       userEmail = "dreems2reality@gmail.com";
 
       # Set Default Branch as main
-      extraConfig = { init.defaultBranch = "main"; };
+      settings = { init.defaultBranch = "main"; };
     };
   };
 }
