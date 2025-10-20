a5627c3ca4de58cf9eec59565198be615c321ff6
PaperMushrooms <dreems2reality@gmail.com>
2025-10-20 03:42:55 -0400

    Update

--- modules/nixos/system/gnome.nix
+++ modules/nixos/system/gnome.nix
@@ -23,7 +23,7 @@ with lib; {
       gnome-control-center
       gnome-tweaks
       gnomeExtensions.desktop-cube
-      gnomeExtensions.blur-my-shell
+      gnomeExtensions.burn-my-windows
     ];
 
     # Enable dconf
