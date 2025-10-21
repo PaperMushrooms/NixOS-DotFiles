085ff4893cbd8b76aa4a12d035c83865056592e7
PaperMushrooms <dreems2reality@gmail.com>
2025-10-20 23:53:19 -0400

    Update

--- hosts/darwin/home/default.nix
+++ hosts/darwin/home/default.nix
@@ -16,7 +16,7 @@
     enable = true;
     shellAliases = {
       rebuild = "sudo darwin-rebuild switch --flake /etc/nix-darwin#darwin";
-      hellnix-update = "cd /etc/nix-darwin && sudo nix flake update && upload && sudo darwin-rebuild switch --flake .#darwin && ssh -t satanix 'cd /etc/nixos/ && git pull && sudo nixos-rebuild switch --flake .#satanix' && ssh -t nixspawn 'cd /etc/nixos && git pull && sudo nixos-rebuild switch --flake .#nixspawn'";
+      hellnix-update = "cd /etc/nix-darwin && nix flake update && upload && sudo darwin-rebuild switch --flake .#darwin && ssh -t satanix 'cd /etc/nixos/ && git pull && sudo nixos-rebuild switch --flake .#satanix' && ssh -t nixspawn 'cd /etc/nixos && git pull && sudo nixos-rebuild switch --flake .#nixspawn'";
 
 hellnix-push = "cd /etc/nix-darwin && upload && sudo darwin-rebuild switch --flake .#darwin && ssh -t satanix 'cd /etc/nixos/ && git pull && sudo nixos-rebuild switch --flake .#satanix' && ssh -t nixspawn 'cd /etc/nixos && git pull && sudo nixos-rebuild switch --flake .#nixspawn'";
 
