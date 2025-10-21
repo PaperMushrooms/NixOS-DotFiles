ccd981af5b477015cc15f50c9a747c1c35952cc6
PaperMushrooms <dreems2reality@gmail.com>
2025-10-20 21:22:41 -0400

    Update

--- shells/PoGo-Root/scripts.nix
+++ shells/PoGo-Root/scripts.nix
@@ -1,36 +1,36 @@
 { pkgs }: [
   (pkgs.writeShellScriptBin "McLaren-crDroid7-A11" ''
-    cd /home/$USER/Android/McLaren/crDroid7-A11/
+    cd McLaren/crDroid7-A11/
     bash install.sh
     cd
   '')
 
   (pkgs.writeShellScriptBin "McLaren-crDroid8-A12" ''
-    cd /home/$USER/Android/McLaren/crDroid8-A12/
+    cd McLaren/crDroid8-A12/
     bash install.sh
     cd
   '')
 
   (pkgs.writeShellScriptBin "McLaren-crDroid9-A13" ''
-    cd /home/$USER/Android/McLaren/crDroid9-A13/
+    cd McLaren/crDroid9-A13/
     bash install.sh
     cd
   '')
 
   (pkgs.writeShellScriptBin "McLaren-crDroid10-A14" ''
-    cd /home/$USER/Android/McLaren/crDroid10-A14/
+    cd McLaren/crDroid10-A14/
     bash install.sh
     cd
   '')
 
   (pkgs.writeShellScriptBin "OnePlus6T-crDroid9-A13" ''
-    cd /home/$USER/Android/OnePlus6T/crDroid9-A13
+    cd OnePlus6T/crDroid9-A13
     bash install.sh
     cd
   '')
 
   (pkgs.writeShellScriptBin "OnePlus7T-crDroid9-A13" ''
-    cd ~/Android/OnePlus7T/crDroid9-A13
+    cd OnePlus7T/crDroid9-A13
     fastboot flash dtbo dtbo.img
     fastboot flash vbmeta vbmeta.img
     fastboot flash recovery_a recovery.img
@@ -50,11 +50,11 @@
 
     Press Enter to Flash Magisk."
     adb sideload ~/Android/Apps/Magisk.apk
-    cd
+    cd ../../
   '')
 
   (pkgs.writeShellScriptBin "OnePlus9-crDroid9-A13" ''
-    cd ~/Android/OnePlus9/crDroid9-A13
+    cd OnePlus9/crDroid9-A13
     bash install.sh
     cd
   '')
@@ -79,13 +79,13 @@
   '')
 
   (pkgs.writeShellScriptBin "Pixel5-crDroid9-A13" ''
-    cd ~/Android/Pixel5/crDroid9-A13
+    cd Pixel5/crDroid9-A13
     bash install.sh
     cd
   '')
 
   (pkgs.writeShellScriptBin "Pixel5-OEM-A13" ''
-    cd ~/Android/Pixel5/OEM-A13-Latest
+    cd Pixel5/OEM-A13-Latest
     bash install.sh
     cd
   '')
@@ -97,7 +97,7 @@
   '')
 
   (pkgs.writeShellScriptBin "Pixel6Pro-OEM-A13" ''
-    cd ~/Android/Pixel6Pro/OEM-A13-Magisk
+    cd Pixel6Pro/OEM-A13-Magisk
     bash install.sh
   '')
 
@@ -106,13 +106,13 @@
   '')
 
   (pkgs.writeShellScriptBin "Pixel7a-crDroid9-A13" ''
-    cd ~/Android/Pixel7a/crDroid9-A13
+    cd Pixel7a/crDroid9-A13
     bash install.sh
     cd
   '')
 
   (pkgs.writeShellScriptBin "Pixel7a-OEM-A13" ''
-    cd ~/Android/Pixel7a/OEM-A13-Latest
+    cd Pixel7a/OEM-A13-Latest
     bash install.sh
     cd
   '')
@@ -127,8 +127,8 @@
     #!/bin/bash
 
     for device in $(adb devices | grep device$ | cut -f1); do
-    sudo adb -s "$device" reboot bootloader
-    sudo fastboot -s "$device" flashing unlock &
+    adb -s "$device" reboot bootloader
+    fastboot -s "$device" flashing unlock &
     done
   '')
 ]
