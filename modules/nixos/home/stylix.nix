836b3572993601fe0e2088004cf0edd2c250565b
PaperMushrooms <dreems2reality@gmail.com>
2025-10-24 10:56:49 -0400

    Update

--- modules/nixos/home/stylix.nix
+++ modules/nixos/home/stylix.nix
@@ -3,7 +3,6 @@
     kde.enable = false;
     kde.decorations.enable = false;
     gtk.enable = false;
-    gtk3.enable = false;
     qt.enable = false;
     firefox.profileNames = [ "default" ];
   };
