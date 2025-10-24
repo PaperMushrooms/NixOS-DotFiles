6aaf04abb045d4dffacacf0f4bf5621c570db5b2
PaperMushrooms <dreems2reality@gmail.com>
2025-10-24 11:15:15 -0400

    Update

--- modules/nixos/home/discord.nix
+++ modules/nixos/home/discord.nix
@@ -11,16 +11,7 @@
         "https://raw.githubusercontent.com/link/to/some/theme.css"
       ];
       frameless = true; # Set some Vencord options
-      plugins = {
-#       hideAttachments.enable = true; # Enable a Vencord plugin
-        ignoreActivities = {
-          # Enable a plugin and set some options
-          enable = true;
-          ignorePlaying = true;
-          ignoreWatching = true;
-          ignoredActivities = [ "someActivity" ];
-        };
-      };
+      plugins = { };
     };
     dorion = {
       theme = "dark";
