292d37bc7e0f64d75cdb692469eb765193cab422
PaperMushrooms <dreems2reality@gmail.com>
2025-10-24 10:52:14 -0400

    Update

--- modules/nixos/home/stylix.nix
+++ modules/nixos/home/stylix.nix
@@ -2,6 +2,9 @@
   stylix.targets = {
     kde.enable = false;
     kde.decorations.enable = false;
+    gtk.enable = false;
+    gtk3.enable = false;
+    qt.enable = false;
     firefox.profileNames = [ "default" ];
   };
 }
